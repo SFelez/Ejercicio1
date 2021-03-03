09be3acb4ec8aa951cf5a4cf83e184264b958f68
Sergio <sergiofelez2001@gmail.com>
2021-03-03 10:45:43 +0100

    Creado el metodo de concatenacion en la clase Metodos

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -3,8 +3,12 @@ package ejercicios;
 public class Metodos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println(concatenacion("hola", "amigo"));
 	}
 
+	public static String concatenacion(String palabra1, String palabra2) {
+		
+		return palabra1 + " " +  palabra2;
+	}
 }
