bed6185826323010f4849be05e35c56bea044c26
Sergio <sergiofelez2001@gmail.com>
2021-03-03 11:02:21 +0100

    Creamos nuevo metodo que caracterDevuelto en la clase Metodos

--- src/ejercicios/Metodos.java
+++ src/ejercicios/Metodos.java
@@ -5,10 +5,17 @@ public class Metodos {
 	public static void main(String[] args) {
 
 		System.out.println(concatenacion("hola", "amigo"));
+		
+		System.out.println(caracterDevuelto(64));
 	}
 
 	public static String concatenacion(String palabra1, String palabra2) {
 		
 		return palabra1 + " " +  palabra2;
+	}
+	
+	public static char caracterDevuelto(int numero) {
+		
+		return (char) numero;
 	}
 }
